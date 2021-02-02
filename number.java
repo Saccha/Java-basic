2603a5fb7344ddf4606948457e3e25054f9ab61e
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-02-01 23:57:52 -0300

    Create number.java

---
+++ number.java
@@ -0,0 +1,17 @@
+public class number {
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 3;
+        int c = a + 1;
+        int d = c - b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(d-a);
+        System.out.println(b-3);
+
+    }
+}
