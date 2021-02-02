4e1d8901fb55dde20ea7dd5010e0b3e3a3a221da
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-02-01 23:58:51 -0300

    Create string.java

---
+++ string.java
@@ -0,0 +1,11 @@
+public class string {
+    public static void main(String[] args) {
+        String a = "I";
+        String b = "learn";
+        String c = "Java";
+        String s = "Learning Java is fun!";
+
+        System.out.println(a + " " + b + " " + c);
+        System.out.println(s.length());
+    }
+}
